b7ba23b89f91afc9b2d3cf9b4715eef6d2f387a5
DarwinB01 <djbonillar@uqvirtual.edu.co>
2021-04-14 18:08:54 -0500

    se realizo el metodo editar en el back

--- src/main/java/com/uniquindio/FincApp/controller/EmployeeController.java
+++ src/main/java/com/uniquindio/FincApp/controller/EmployeeController.java
@@ -2,13 +2,18 @@ package com.uniquindio.FincApp.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,25 +29,49 @@ public class EmployeeController {
 
 	@Autowired
 	private IEmployeeService employeeService;
-	
+
 	@PostMapping("/trabajadores")
 	public EmployeeDTO saveEmployee(@RequestBody EmployeeDTO employee) {
 		employee.setCreateAt(new Date());
 		return employeeService.saveEmployee(employee);
 	}
-	
+
 	@DeleteMapping("/trabajadores/{cedula}")
 	public void deleteEmployeeById(@PathVariable Long cedula) {
 		employeeService.deleteById(cedula);
 	}
-	
+
 	@GetMapping("/trabajadores/{cedula}")
 	public EmployeeDTO findEmployee(@PathVariable Long cedula) {
 		return employeeService.findById(cedula);
 	}
-	
+
 	@GetMapping("/trabajadores")
-	public List<EmployeeDTO> findAllEmployee(){
+	public List<EmployeeDTO> findAllEmployee() {
 		return employeeService.findAll();
+	}
+
+	@PutMapping("/trabajadores/{cedula}")
+	public EmployeeDTO updateEmployee(@RequestBody EmployeeDTO peticion, @PathVariable Long cedula) {
+
+		EmployeeDTO employeeDTO = employeeService.findById(cedula);
+
+		EmployeeDTO employeeUpdated = null;
+		if (employeeDTO != null) {
+
+			try {
+				employeeDTO.setNombre(peticion.getNombre());
+				employeeDTO.setApellido(peticion.getApellido());
+				employeeDTO.setSueldo(peticion.getSueldo());
+				employeeDTO.setEmail(peticion.getEmail());
+				employeeDTO.setCultivo(peticion.getCultivo());
+
+				employeeUpdated = employeeService.saveEmployee(employeeDTO);
+				return employeeUpdated;
+			} catch (DataAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		return employeeUpdated;
 	}
 }
