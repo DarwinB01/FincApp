1e96ecb2bad8a27de28f81f28cbd6e327537ea97
DarwinB01 <djbonillar@uqvirtual.edu.co>
2021-04-19 23:33:18 -0500

    solucion de conflictos

--- src/main/java/com/uniquindio/FincApp/model/User.java
+++ src/main/java/com/uniquindio/FincApp/model/User.java
@@ -18,10 +18,10 @@ import com.sun.istack.NotNull;
 
 /**
  *Clase que representa a un usuario dentro de la finca
- *Para iniciar sesion
+ *
  *
  * @author Darwin Bonilla
- * fg
+ * 
  */
 
 @Entity
